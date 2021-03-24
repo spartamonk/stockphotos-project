72e7192d97c6b029aa4a2a3a702a56d5de77c62b
spartamonk <cm.akoto@yahoo.com>
2021-03-24 15:44:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query,setQuery] = useState('');
+  const fetchImages = async()=> {
+    setIsLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results
+        }
+        else if(query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+      })
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
+  useEffect(()=> {
+    fetchImages()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[page])
+
+  // unlimited scroll
+  useEffect(()=> {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        (!isLoading && window.innerHeight + window.scrollY) >=
+        document.body.scrollHeight - 2
+      ) {
+        setPage((oldValue) => {
+          return oldValue + 1
+        })
+      }
+    })
+    return () => window.removeEventListener('scroll', event)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  const handleSubmit =(e)=> {
+    e.preventDefault();
+    setPage(1);
+  }
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input
+            type='text'
+            placeholder='search'
+            className='form-input'
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value)
+            }}
+          />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />
+          })}
+        </div>
+        {isLoading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
+}
+export default App
